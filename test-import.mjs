c6a2756077867b761f289dece0453e14ecef9e1e
Denis <dstrekha@gmail.com>
2026-01-16 11:24:53 +0000

    Cleanup: remove unnecessary files, update project structure

--- test-import.mjs
+++
@@ -1,5 +0,0 @@
-import('@maxhub/max-ui').then(module => {
-  console.log('Button доступен?', typeof module.Button);
-  console.log('Panel доступен?', typeof module.Panel);
-  console.log('Typography доступен?', typeof module.Typography);
-}).catch(err => console.error('Ошибка импорта:', err));
